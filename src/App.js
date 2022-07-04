a61b7f27b395f98052a6e9fcfd39daf235b0cc24
Rajat Tomar <rajat.rajattomar@gmail.com>
2022-07-04 07:54:15 +0530

    Call components with props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Expense from "./components/expense/Expense";
+import CreateExpense from "./components/expense/CreateExpense";
+import {GetExpenses} from "./components/expense/GetExpenses";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 function App() {
   return (
       <>
-          <Expense />
+          <CreateExpense BASE_URL={BASE_URL}/>
+          <GetExpenses BASE_URL={BASE_URL}/>
       </>
   );
 }
