9b11ba45dc89bbb1f6909c9db2af2b963aaa375e
Rajat Tomar <rajat.rajattomar@gmail.com>
2022-07-06 17:29:47 +0530

    Update footer to make it sticky

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,14 +2,17 @@ import {Link} from "react-router-dom";
 
 export const Footer = () => {
     return (<>
-        <footer className="text-center bg-gray-900 text-white">
+        <footer className="sticky fixed
+             inset-x-0
+             bottom-0
+             text-center bg-gray-900 text-white">
             <div className="container mx-auto px-4 py-8">
                 <div className="flex flex-col items-center">
                     <Link to="/" className="text-white">
                         <img className="h-8 w-auto"
-                                src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="workflow"/>
+                             src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="workflow"/>
                     </Link>
-                    <p  className="p-4">
+                    <p className="p-4">
                         © 2022 Copyright: GaussB Labs
                     </p>
                 </div>
