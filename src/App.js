3bc81810ebf23b7db51636ce2cc2b8fae5f78625
Rajat Tomar <rajat.rajattomar@gmail.com>
2022-07-15 15:07:51 +0530

    Use reducer to update routes as per logged in and logged out state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import React from "react";
-import {GetExpenses} from "./components/expense/GetExpenses";
-import {Header} from "./components/Header";
-import {Footer} from "./components/Footer";
+import {createContext, useReducer} from 'react';
 import {BrowserRouter as Router, Route, Routes,} from "react-router-dom";
+import Navbar from './components/Navbar';
+import {initialState, reducer} from './reducer/UseReducer';
+import Login from './components/Login';
+import Logout from './components/Logout';
 import {CreateExpense} from "./components/expense/CreateExpense";
-import {Login} from "./components/Login";
+import {GetExpenses} from "./components/expense/GetExpenses";
+
+export const UserContext = createContext(undefined);
+
+const Routing = ({state}) => {
+    if (state) {
+        return (<Routes>
+            <Route path="/logout" element={<Logout/>}/>
+            <Route path="/create-expense" element={<CreateExpense/>}/>
+            <Route path="/expenses" element={<GetExpenses/>}/>
+        </Routes>);
+    } else {
+        return (<Routes>
+            <Route path="/" element={<Login/>}/>
+        </Routes>);
+    }
+}
 
 function App() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
     return (<>
-        <Router>
-            <Header/>
-            <Routes>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/create_expense" element={<CreateExpense/>}/>
-                <Route path="/expenses" element={<GetExpenses/>}/>
-            </Routes>
-            <Footer/>
-        </Router>
-    </>)
+        <UserContext.Provider value={{state, dispatch}}>
+            <Router>
+                <Navbar/>
+                <Routing state={state}/>
+            </Router>
+        </UserContext.Provider>
+    </>);
 }
 
 export default App;
