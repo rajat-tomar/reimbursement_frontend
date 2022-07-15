81cd210252f29dcdd8f275844a1b1afb3ddebbfe
Rajat Tomar <rajat.rajattomar@gmail.com>
2022-07-15 14:57:57 +0530

    Add logout component

---
+++ src/components/Logout.js
@@ -0,0 +1,22 @@
+import React, {useContext, useEffect} from 'react';
+import {UserContext} from '../App';
+import {useNavigate} from 'react-router-dom';
+
+const Logout = () => {
+    const {state, dispatch} = useContext(UserContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (state) {
+            dispatch({type: "USER", payload: false});
+            localStorage.clear()
+            navigate("/");
+        }
+    }, []);
+
+    return (<div>
+
+    </div>);
+};
+
+export default Logout;
