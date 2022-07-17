7d1781acff9843fd21fc65eaf656c2575d933bec
Rajat Tomar <rajat.rajattomar@gmail.com>
2022-07-17 21:00:35 +0530

    list expenses by selecting users for admin and ca

--- src/components/expense/GetExpenses.js
+++ src/components/expense/GetExpenses.js
@@ -4,6 +4,7 @@ import {BASE_URL} from "../../constants";
 export const GetExpenses = () => {
     const [expenses, setExpenses] = useState([])
     const [users, setUsers] = useState([])
+    const [userId, setUserId] = useState("")
     const [category, setCategory] = useState("")
     const [dateRange, setDateRange] = useState({
         startDate: "", endDate: ""
@@ -13,17 +14,6 @@ export const GetExpenses = () => {
     const handleChange = (e) => {
         const value = e.target.value
         setDateRange({...dateRange, [e.target.name]: value})
-    }
-
-    const submit = (e) => {
-        e.preventDefault()
-        if (dateRange.startDate !== "" && dateRange.endDate === "") {
-            alert("please select both start date and end date")
-        } else if ((dateRange.startDate === "" && dateRange.endDate !== "")) {
-            alert("please select both start date and end date")
-        } else {
-            getExpenses()
-        }
     }
 
     const getUsers = () => {
@@ -41,7 +31,7 @@ export const GetExpenses = () => {
     }
 
     const getExpenses = () => {
-        fetch(`${BASE_URL}/expenses?category=${category}&startDate=${dateRange.startDate}&endDate=${dateRange.endDate}`, {
+        fetch(`${BASE_URL}/expenses?category=${category}&startDate=${dateRange.startDate}&endDate=${dateRange.endDate}&userId=${userId}`, {
             method: 'GET', headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
@@ -67,10 +57,10 @@ export const GetExpenses = () => {
 
     useEffect(() => {
         getExpenses()
-    })
+    }, [dateRange, userId, category])
 
     return (<>
-        <form onSubmit={submit}>
+        <form>
             <div className="flex flex-col items-center">
                 <div className="flex date-range-picker">
                     <div className="date-range-picker__input">
@@ -109,14 +99,13 @@ export const GetExpenses = () => {
                             User</label>
                         <select id="users"
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                value={category} onChange={() => ""}>
+                                value={userId} onChange={(e) => setUserId(e.target.value)}>
                             <option value="">Choose a user</option>
                             {users ? users.map((user, index) => {
                                 return <option key={index} value={user.id}>{user.name} - {user.email}</option>
                             }) : null}
                         </select></> : null}
                 </div>
-                <button type="submit">Submit</button>
                 <div className="p-4 bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
                     <div className="flex justify-between items-center mb-4">
                         <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Your Expenses</h5>
