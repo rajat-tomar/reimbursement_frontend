3230a3f50dafe32510f74e454d7c78df0b247384
Rajat Tomar <rajat.rajattomar@gmail.com>
2022-07-21 16:22:22 +0530

    Add code to get expenses by date range

--- src/components/reimbursement/GetReimbursements.js
+++ src/components/reimbursement/GetReimbursements.js
@@ -7,9 +7,17 @@ export const GetReimbursements = () => {
     const currentUser = JSON.parse(localStorage.getItem("user"));
     const [users, setUsers] = useState([])
     const [userId, setUserId] = useState("")
+    const [dateRange, setDateRange] = useState({
+        startDate: "", endDate: ""
+    })
+
+    const handleDate = (e) => {
+        const value = e.target.value
+        setDateRange({...dateRange, [e.target.name]: value})
+    }
 
     const getReimbursements = () => {
-        fetch(`${BASE_URL}/reimbursements?userId=${userId}`, {
+        fetch(`${BASE_URL}/reimbursements?userId=${userId}&startDate=${dateRange.startDate}&endDate=${dateRange.endDate}`, {
             method: 'GET', headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
@@ -27,9 +35,7 @@ export const GetReimbursements = () => {
     const calculateTotal = () => {
         let total = 0
         reimbursements?.forEach(reimbursement => {
-            if (reimbursement.status === "pending") {
-                total += reimbursement.amount
-            }
+            total += reimbursement.amount
         })
         setTotal(total)
     }
@@ -67,7 +73,7 @@ export const GetReimbursements = () => {
 
     useEffect(() => {
         getReimbursements()
-    }, [userId])
+    }, [userId, dateRange])
 
     useEffect(() => {
         calculateTotal()
@@ -79,6 +85,18 @@ export const GetReimbursements = () => {
 
     return (<>
         <div className="flex flex-col items-center">
+            <div className="flex date-range-picker">
+                <div className="date-range-picker__input">
+                    <label htmlFor="start-date">Start Date</label>
+                    <input type="date" id="start-date" name="startDate"
+                           onChange={handleDate}/>
+                </div>
+                <div className="w-1/2 date-range-picker__input">
+                    <label htmlFor="end-date">End Date</label>
+                    <input type="date" id="end-date" name="endDate"
+                           onChange={handleDate}/>
+                </div>
+            </div>
             {(currentUser.role === "admin" || currentUser.role === "ca") && (<>
                 <label htmlFor="users"
                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Select
@@ -95,25 +113,36 @@ export const GetReimbursements = () => {
             </>)}
             <div className="p-4 bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
                 <div className="flex justify-between items-center mb-4">
-                    <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Pending
-                        Reimbursements</h5>
+                    {(dateRange.startDate === "" && dateRange.endDate === "") ? (<>
+                        <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Pending
+                            Reimbursements</h5>
+                    </>) : (<>
+                        <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Processed
+                            Reimbursements</h5>
+                    </>)}
                 </div>
                 <div className="flow-root">
                     <ul className="divide-y divide-gray-200 dark:divide-gray-700">
                         {reimbursements?.map((reimbursement, index) => {
-                            return (reimbursement.status === "pending" && (<li key={index} className="py-3 sm:py-4">
+                            const date = new Date(reimbursement.processed_on)
+                            return (<li key={index} className="py-3 sm:py-4">
                                 <div className="flex items-center space-x-4">
                                     <div className="flex-1 min-w-0">
                                         <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
                                             {reimbursement.category}
                                         </p>
+                                        {reimbursement.status === "approved" && (<>
+                                            <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                                                {date.toDateString()}
+                                            </p>
+                                        </>)}
                                     </div>
                                     <div
                                         className="items-center text-base font-semibold text-gray-900 dark:text-white">
                                         <span>₹{reimbursement.amount}</span>
                                     </div>
                                 </div>
-                            </li>))
+                            </li>)
                         })}
                         <li className="py-3 sm:py-4">
                             <div className="flex items-center space-x-4">
@@ -124,7 +153,7 @@ export const GetReimbursements = () => {
                                     className="items-center text-base font-semibold text-gray-900 dark:text-white">
                                     <span>₹{total}</span>
                                     <br/>
-                                    {((currentUser.role === "admin" || currentUser.role === "ca") && (total > 0)) && (
+                                    {((currentUser.role === "admin" || currentUser.role === "ca") && (dateRange.startDate === "" && dateRange.endDate === "") && (total > 0)) && (
                                         <button
                                             className="font-medium text-blue-600 hover:underline dark:text-blue-500"
                                             onClick={() => handleProcess()}> Process
